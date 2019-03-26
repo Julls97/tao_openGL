685f07edf690f591356dfcd9610eb57eeaf9eba1
jull <yulia.jolie@yandex.ru>
2019-03-26 16:04:21 +0500

    lab2

--- openGL/Form1.Designer.cs
+++ openGL/Form1.Designer.cs
@@ -28,43 +28,51 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.anT = new Tao.Platform.Windows.SimpleOpenGlControl();
-            this.SuspendLayout();
-            // 
-            // anT
-            // 
-            this.anT.AccumBits = ((byte)(0));
-            this.anT.AutoCheckErrors = false;
-            this.anT.AutoFinish = false;
-            this.anT.AutoMakeCurrent = true;
-            this.anT.AutoSwapBuffers = true;
-            this.anT.BackColor = System.Drawing.Color.Black;
-            this.anT.ColorBits = ((byte)(32));
-            this.anT.DepthBits = ((byte)(16));
-            this.anT.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.anT.Location = new System.Drawing.Point(0, 0);
-            this.anT.Name = "anT";
-            this.anT.Size = new System.Drawing.Size(541, 450);
-            this.anT.StencilBits = ((byte)(0));
-            this.anT.TabIndex = 0;
-            this.anT.KeyUp += new System.Windows.Forms.KeyEventHandler(this.anT_KeyUp);
-            // 
-            // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(541, 450);
-            this.Controls.Add(this.anT);
-            this.Name = "Form1";
-            this.Text = "Form1";
-            this.Resize += new System.EventHandler(this.Form1_Resize);
-            this.ResumeLayout(false);
+			this.components = new System.ComponentModel.Container();
+			this.anT = new Tao.Platform.Windows.SimpleOpenGlControl();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			this.SuspendLayout();
+			// 
+			// anT
+			// 
+			this.anT.AccumBits = ((byte)(0));
+			this.anT.AutoCheckErrors = false;
+			this.anT.AutoFinish = false;
+			this.anT.AutoMakeCurrent = true;
+			this.anT.AutoSwapBuffers = true;
+			this.anT.BackColor = System.Drawing.Color.Black;
+			this.anT.ColorBits = ((byte)(32));
+			this.anT.DepthBits = ((byte)(16));
+			this.anT.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.anT.Location = new System.Drawing.Point(0, 0);
+			this.anT.Name = "anT";
+			this.anT.Size = new System.Drawing.Size(541, 450);
+			this.anT.StencilBits = ((byte)(0));
+			this.anT.TabIndex = 0;
+			this.anT.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.anT_KeyPress);
+			this.anT.KeyUp += new System.Windows.Forms.KeyEventHandler(this.anT_KeyUp);
+			// 
+			// timer1
+			// 
+			this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(541, 450);
+			this.Controls.Add(this.anT);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.Resize += new System.EventHandler(this.Form1_Resize);
+			this.ResumeLayout(false);
 
         }
 
         #endregion
 
         private Tao.Platform.Windows.SimpleOpenGlControl anT;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- openGL/Form1.cs
+++ openGL/Form1.cs
@@ -6,144 +6,118 @@ using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
 
-using Tao.OpenGl; 
-using Tao.FreeGlut; 
+using Tao.OpenGl;
+using Tao.FreeGlut;
 using Tao.Platform.Windows;
 
-namespace openGL
-{
-    public partial class Form1 : Form
-    {
-        public Form1()
-        {
-            InitializeComponent();
-            anT.InitializeContexts();
-            InitGL();
-            ResizeGLScene();
-            DrawGLScene();
-        }
+namespace openGL {
+	public partial class Form1 : Form {
+		static float rotZ = 0.0f;
+		static float X = 0.0f;       
+		static float Y = 0.0f;       
 
-        private void InitGL()
-        {
-            Glut.glutInit();
-            Glut.glutInitDisplayMode(Glut.GLUT_RGB | Glut.GLUT_DOUBLE | Glut.GLUT_DEPTH);
-        }
+		public Form1() {
+			InitializeComponent();
+			anT.InitializeContexts();
+			InitGL();
+			ResizeGLScene();
+			DrawGLScene();
+		}
 
-        /*
-        //private void Form1_Load(object sender, EventArgs e)
-        //{
-        //// инициализация Glut 
-        //Glut.glutInit();
-        //Glut.glutInitDisplayMode(Glut.GLUT_RGB | Glut.GLUT_DOUBLE | Glut.GLUT_DEPTH);
-        //// отчитка окна 
-        //Gl.glClearColor(255, 255, 255, 1);
-        //// установка порта вывода в соответствии с размерами элемента anT 
-        //Gl.glViewport(0, 0, anT.Width, anT.Height);
-        //// настройка проекции 
-        //Gl.glMatrixMode(Gl.GL_PROJECTION);
-        //Gl.glLoadIdentity();
-        //Glu.gluPerspective(45, (float)anT.Width / (float)anT.Height, 0.1, 200);
-        //Gl.glMatrixMode(Gl.GL_MODELVIEW);
-        //Gl.glLoadIdentity();
-        //// настройка параметров OpenGL для визуализации 
-        //Gl.glEnable(Gl.GL_DEPTH_TEST);
-        // }
+		private void InitGL() {
+			Glut.glutInit();
+			Glut.glutInitDisplayMode(Glut.GLUT_RGB | Glut.GLUT_DOUBLE | Glut.GLUT_DEPTH);
+		}
 
-        //private void button1_Click(object sender, EventArgs e)
-        //{
-        //    Gl.glClear(Gl.GL_COLOR_BUFFER_BIT | Gl.GL_DEPTH_BUFFER_BIT);
-        //    Gl.glLoadIdentity();
-        //    Gl.glColor3f(1.0f, 0, 0);
-        //    Gl.glPushMatrix();
-        //    Gl.glTranslated(0, 0, -6);
-        //    Gl.glRotated(45, 1, 1, 0);
+		private void ResizeGLScene() {
+			if (anT.Height == 0) {
+				anT.Height = 1;
+			}
 
-        //    // рисуем сферу с помощью библиотеки FreeGLUT 
-        //    Glut.glutWireSphere(2, 32, 32);
-        //    Gl.glPopMatrix();
-        //    Gl.glFlush();
-        //    simpleOpenGlControl1.Invalidate();
-        //}
+			Gl.glViewport(0, 0, anT.Width, anT.Height);
+			Gl.glMatrixMode(Gl.GL_PROJECTION);
+			Gl.glLoadIdentity();
+			Glu.gluPerspective(45.0f, (float)anT.Width / (float)anT.Height, 0.1f, 200.0f);
+			Gl.glMatrixMode(Gl.GL_MODELVIEW);
+		}
 
-        //private void button2_Click(object sender, EventArgs e)
-        //{
-        //    Application.Exit();
-        //}
+		private void DrawGLScene() {
+			Gl.glClearColor(1, 1, 1, 1.0f);
+			Gl.glClear(Gl.GL_COLOR_BUFFER_BIT | Gl.GL_DEPTH_BUFFER_BIT);
+			Gl.glLoadIdentity();
 
-        //private void simpleOpenGlControl1_Paint(object sender, PaintEventArgs e)
-        //{
-        //устанвливем в черный цвет 
+			Gl.glPushMatrix();
+			Gl.glTranslated(0, 0, -4);
 
-        //Tao.OpenGl.Gl.glClearColor(0, 0, 0, 0);
+			Gl.glColor3f(0.25f, 0.87f, 0.81f);
+			Gl.glBegin(Gl.GL_QUADS);
+			Gl.glVertex2f(X + 1.6f, Y + 1.6f);
+			Gl.glVertex2f(X + 1.1f, Y + 1.6f);
+			Gl.glVertex2f(X + 1.1f, Y + 1.1f);
+			Gl.glVertex2f(X + 1.6f, Y + 1.1f);
+			Gl.glEnd();
 
-        //сообщаем что нужно очистить два буфера 
+			Gl.glColor3f(0.6549f, 0.3765f, 0.4196f);
+			Gl.glRotatef(rotZ, 0.0f, 0.0f, 1.0f);
+			Gl.glBegin(Gl.GL_QUADS);
+			Gl.glVertex2f(1.0f, 1.0f);
+			Gl.glVertex2f(-1.0f, 1.0f);
+			Gl.glVertex2f(-1.0f, -1.0f);
+			Gl.glVertex2f(1.0f, -1.0f);
+			Gl.glEnd();
 
-        //Tao.OpenGl.Gl.glClear(
+			Gl.glPopMatrix();
+			Gl.glFlush();
+			anT.Invalidate();
+		}
 
-        //Tao.OpenGl.Gl.GL_COLOR_BUFFER_BIT | //очистка буфера цвета
+		private void Form1_Resize(object sender, EventArgs e) {
+			ResizeGLScene();
+			DrawGLScene();
+		}
 
-        //Tao.OpenGl.Gl.GL_DEPTH_BUFFER_BIT //очистка буфера глубины
+		private void anT_KeyUp(object sender, KeyEventArgs e) {
+			
+			// Нажата клавиша ЕSCAPE; 
+			if (e.KeyCode == Keys.Escape) {
+				// Завершим приложение; 
+				this.Close();
+			}
 
-        //);
+			// Нажата клавиша right квадрат вращается; 
+			if (e.KeyCode == Keys.Right) {
+				timer1.Start();
+			}
 
-        //перерисовываем окно
+			// Нажата клавиша left вращение прекращается;
+			if (e.KeyCode == Keys.Left) {
+				timer1.Stop();
+			}
+		}
 
-        //anT.Invalidate();
-        // }
-        */
+		private void timer1_Tick(object sender, EventArgs e) {
+			rotZ -= 1.0f;
+			DrawGLScene();
+		}
 
-        private void ResizeGLScene()
-        {
-            if (anT.Height == 0)
-            {
-                anT.Height = 1;
-            }
+		private void anT_KeyPress(object sender, KeyPressEventArgs e) {
+			if (e.KeyChar == 'A' || e.KeyChar == 'a') {
+				X -= 0.1f;
+			}
 
-            Gl.glViewport(0, 0, anT.Width, anT.Height);
-            Gl.glMatrixMode(Gl.GL_PROJECTION);
-            Gl.glLoadIdentity();
-            Glu.gluPerspective(45.0f, (float)anT.Width / (float)anT.Height, 0.1f, 200.0f);
-            Gl.glMatrixMode(Gl.GL_MODELVIEW);
-            Gl.glLoadIdentity();
-        }
+			if (e.KeyChar == 'D' || e.KeyChar == 'd') {
+				X += 0.1f;
+			}
 
-        private void DrawGLScene()
-        {
-            Gl.glClearColor(1, 1, 1, 1.0f);
-            Gl.glClear(Gl.GL_COLOR_BUFFER_BIT | Gl.GL_DEPTH_BUFFER_BIT);
-            Gl.glLoadIdentity();
+			if (e.KeyChar == 'S' || e.KeyChar == 's') {
+				Y -= 0.1f;
+			}
 
-            //Gl.glColor3f(0.5f, 0, 1);
-            Gl.glColor3f(0.6549f, 0.3765f, 0.4196f);
-            Gl.glPushMatrix();
-            Gl.glTranslated(0, 0, -6);
+			if (e.KeyChar == 'W' || e.KeyChar == 'w') {
+				Y += 0.1f;
+			}
 
-            Gl.glBegin(Gl.GL_QUADS);
-            Gl.glVertex3f(1.0f, 1.0f, 0.0f);
-            Gl.glVertex3f(-1.0f, 1.0f, 0.0f);
-            Gl.glVertex3f(-1.0f, -1.0f, 0.0f);
-            Gl.glVertex3f(1.0f, -1.0f, 0.0f);
-            Gl.glEnd();
-
-            Gl.glPopMatrix();
-            Gl.glFlush();
-            anT.Invalidate();
-        }
-
-        private void Form1_Resize(object sender, EventArgs e)
-        {
-            ResizeGLScene();
-            DrawGLScene();
-        }
-
-        private void anT_KeyUp(object sender, KeyEventArgs e)
-        {
-            // Нажата клавиша ЕSCAPE; 
-            if (e.KeyCode == Keys.Escape) {
-                // Завершим приложение; 
-                this.Close();
-            }
-            
-        }
-    }
+			DrawGLScene();
+		}
+	}
 }
